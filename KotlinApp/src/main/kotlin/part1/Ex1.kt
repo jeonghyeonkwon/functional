c61b289fdff9d8ea5746654ca73d824fa0b3ed1d
jeonghyeonkwon <givejeong2468@gmail.com>
2023-02-09 07:15:36 +0900

    ES6 리스트 순회

---
+++ KotlinApp/src/main/kotlin/part1/Ex1.kt
@@ -0,0 +1,17 @@
+/*
+const add5 = (a) => a + 5;
+console.log(add5); // a => a + 5
+console.log(add5(5)); // 10
+
+const f1 = () => () => 1;
+console.log(f1()); // () => 1
+
+const f2 = f1();
+console.log(f2); // () => 1
+log(f2()); // 1
+* */
+
+fun main(){
+    val add5 = (a :Int) ->
+}
+
