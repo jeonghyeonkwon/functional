52e39470012dc3d21e582907f97dfb8b8d5aa015
jeonghyeonkwon <givejeong2468@gmail.com>
2023-02-22 19:33:41 +0900

    코틀린에서 list, set, map 정리

--- KotlinApp/src/main/kotlin/part2/Ex.kt
+++ KotlinApp/src/main/kotlin/part2/Ex.kt
@@ -1,10 +1,13 @@
 package part2
 
 fun main(){
-    es6에서_리스트_순회()
+//    코틀린_배열()
+//    코틀린_list()
+//    코틀린_set()
+    코틀린_map()
 }
 
-fun es6에서_리스트_순회(){
+fun 코틀린_배열(){
     val arr = arrayOf<Int>(1,2,3,4,5);
 
 
@@ -15,4 +18,32 @@ fun es6에서_리스트_순회(){
     for(i in arr){
         print(i)
     }
+}
+
+fun 코틀린_list(){
+    var list = mutableListOf<Int>(1,2,3);
+    list.add(4);
+
+    for(i in 0 until list.size){
+        print(list[i]);
+    }
+    println()
+    for(i in list){
+        print(i)
+    }
+}
+
+fun 코틀린_set(){
+    var set = mutableSetOf<Int>(1,2,3,4,4);
+
+    set.add(5);
+    print(set)
+}
+fun 코틀린_map(){
+    var map = mutableMapOf<String,Int>("a" to 1, "b" to 2, "c" to 3);
+    map.put("d" , 4);
+    for((key,value) in map){
+        println("key : " + key + " value : " + value)
+    }
+
 }
