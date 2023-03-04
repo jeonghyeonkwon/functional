7a69a977c264bf1395048ff0a839f67fee6bd338
jeonghyeonkwon <givejeong2468@gmail.com>
2023-03-04 13:20:20 +0900

    코틀린 fFilter 만들기

--- KotlinApp/src/main/kotlin/part4/Ex.kt
+++ KotlinApp/src/main/kotlin/part4/Ex.kt
@@ -7,10 +7,16 @@ fun main(){
     products.add(Product("핸드폰 케이스",15000))
     products.add(Product("후드티",30000))
     products.add(Product("바지",25000))
+    커스텀_필터(products)
 }
 
-fun 커스텀_필터(product: Product){
-    fFilter({ p -> p.price < 2000 } , product)
+fun 커스텀_필터(products: List<Product>){
+    val result:List<Product> = fFilter({ p -> p.price < 20000 } , products)
+    result.forEach { res -> println(res.name)}
+
+    val result2:List<Int> = fFilter({n -> n % 2==0}, mutableListOf(1,2,3,4,5,6,7,8,9,10))
+    result2.forEach {res -> println(res)}
+
 }
 
 fun <T>fFilter(fn : (T) -> Boolean , list:List<T>):List<T>{
